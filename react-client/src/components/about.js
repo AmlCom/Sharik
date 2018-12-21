d40825ea1f36266a02992e1b3e81870a3ea132d8
mustafcode10 <mustafdiiriye@hotmail.com>
2018-12-22 01:07:38 +0200

    feat signup contact and about

--- react-client/src/components/about.js
+++ react-client/src/components/about.js
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from 'react-router-dom'
 
 const styles = {
   root: {
@@ -22,38 +21,30 @@ const styles = {
   },
 };
 
-function Nav(props) {
+function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      {/* <AppBar position="static">
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.grow}>
-          <Link to= '/'>Home</Link>
+            News
           </Typography>
-          <Button color="inherit">
-          <Link to='/signin'>Signin</Link>
-          </Button>
-          <Button color="inherit">
-          <Link to ='/signup'>Signup</Link>
-          </Button>
-          <Button color="inherit">
-          <Link to ='/contact'>Contact</Link>
-          </Button>
-          <Button color="inherit">
-          <Link to ='/about'>About</Link>
-          </Button>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Log out</Button>
+          <Button color="inherit">About</Button>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
+      <p>Hello word </p>
     </div>
   );
 }
 
-Nav.propTypes = {
+ButtonAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Nav);
+export default withStyles(styles)(ButtonAppBar);
---
+++ react-client/src/components/contact.js
@@ -0,0 +1,107 @@
+
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import { withStyles } from '@material-ui/core/styles';
+
+
+
+const styles = theme => (
+    {
+        main: {
+            width: 'auto',
+            display: 'block', // Fix IE 11 issue.
+            marginLeft: theme.spacing.unit * 3,
+            marginRight: theme.spacing.unit * 3,
+            [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+                width: 400,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            },
+        },
+        root: {
+            ...theme.mixins.gutters(),
+            paddingTop: theme.spacing.unit * 2,
+            paddingBottom: theme.spacing.unit * 2,
+          },
+        paper: {
+            marginTop: theme.spacing.unit * 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+        },
+        avatar: {
+            margin: theme.spacing.unit,
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '60%', // Fix IE 11 issue.
+            marginTop: theme.spacing.unit,
+        },
+        submit: {
+            marginTop: theme.spacing.unit * 3,
+        },
+    }
+);
+
+class Contact extends Component {
+    state = {
+        sent: false,
+      };
+    
+
+    render(){
+        console.log(styles)
+        const { classes } = this.props;
+        return (
+        <div className="Contact">
+        <main className={classes.main}>
+            <Paper className={classes.root} elevation={1}>
+            <CssBaseline />
+            <AppBar position="static">
+                </AppBar> 
+                <h1 align="center" > Contact Support </h1>
+                <form className={classes.form}>
+                <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Title</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="username">Please describe your problem</InputLabel>
+                                <Input   id="standard-multiline-static"
+        //   label="Please describe your problem"
+          multiline
+          rows="4"
+          margin="normal" />
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Submit
+                    </Button>
+                </form>
+                </Paper>
+                </main>
+        </div>
+    
+    )
+    }
+}
+
+
+export default withStyles(styles)(Contact);
