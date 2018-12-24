c99f3526e369accda5ca0fee9f32247ab4f861ed
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-24 17:07:53 +0200

    (feat) create

--- react-client/src/components/App/App.js
+++ react-client/src/components/App/App.js
@@ -9,7 +9,7 @@ import Contact from '../contact'
 import Nav from '../Nav'
 import Teachers from '../Teachers/Teachers';
 import Teacher from '../Teachers/Teacher';
-
+import CreateTeacher from '../CreateTeacher'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // import TabContainer from '../Footer/footer'
@@ -47,6 +47,7 @@ class App extends Component {
               <Route path ='/teachers' exact component = {Teachers} />
               <Route path='/profile' exact component={Profile} />
               <Route path='/Teacher' exact component={Teacher}/>
+              <Route path= '/CreateTeacher' exact component={CreateTeacher}/>
             </Switch>
           </div>
 
