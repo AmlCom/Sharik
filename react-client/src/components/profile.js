00a908b67d130b8c7744b77f0ee3cc05b6f597d8
Yahyarashid <yahyomar63@gmail.com>
2018-12-26 17:39:43 +0200

    update profile page

--- react-client/src/components/profile.js
+++ react-client/src/components/profile.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
+import { storage } from '../firebase/index'
 
-// class  Profile extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isLoggedin: false
-//         }
-//     }
-//     render() {
-//         return (
-//             <div>
-//             <h1>This is the Profile page </h1>
-//             {/* <p>{this.props.location.state.referrer}</p> */}
-//             </div>
-//         )
-//     }
 
-const Profile = () => {
+class Profile extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNimrjRZN3jIxU-D90rCnZ6pZvp9QF4t55QWP6PdOrCcyWpsvT"
+        }
+    }
+
+
+    uploadImage = (e) => {
+        //console.log('image',e.target.files[0]);
+        this.setState({
+            image: e.target.files[0]
+        })
+    }
+    submitImage = () => {
+        var image = this.state.image
+    const uploadTask = storage.ref(`images/${image.name}`).put(image)
+    uploadTask.on('state_changed', (snapshot) => {
+
+}, (error) => {
+    console.log(error)
+}, () => {
+    storage.ref('images').child(image.name).getDownloadURL().then(url => {
+        console.log('url',url)
+        this.setState({
+          image:url
+        })
+     //request to the database update the profile picture
+    })
+});
+
+
+    }
+
+
+    render(){
+        console.log('state',this.state.image)
     return (
-
 
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3"  >
 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNimrjRZN3jIxU-D90rCnZ6pZvp9QF4t55QWP6PdOrCcyWpsvT" alt="" class="rounded" />
 
-                    <br />
-                    <br />
-
+                <img src={this.state.image} alt="" class="rounded"/>
+                <br />
+                <br />
+                <input  type='file' onChange = {this.uploadImage}/><br></br>
+                   <button onClick={this.submitImage}>Upload</button>
                     <div class="list-group">
                         <a href="" class="list-group-item active main-color-bg">
                             <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> The Name of the user
@@ -40,11 +63,7 @@ const Profile = () => {
 
 
 
-
-
-
-
-                </div>
+             </div>
                 <div class="col-md-9">
                     <div class="panel panel-default">
                         <div class="panel-heading ">
@@ -88,6 +107,7 @@ const Profile = () => {
             </div>
         </div>
     )
+}
 }
 
 export default Profile;
