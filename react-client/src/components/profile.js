7876c869cf349721b9a102e954e76e8ee5006898
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-26 17:19:41 +0200

    (feat) teacher schema

--- react-client/src/components/profile.js
+++ react-client/src/components/profile.js
@@ -1,20 +1,5 @@
 import React, { Component } from 'react';
 
-// class  Profile extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isLoggedin: false
-//         }
-//     }
-//     render() {
-//         return (
-//             <div>
-//             <h1>This is the Profile page </h1>
-//             {/* <p>{this.props.location.state.referrer}</p> */}
-//             </div>
-//         )
-//     }
 
 const Profile = () => {
     return (
