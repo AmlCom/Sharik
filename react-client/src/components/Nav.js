63715dc837714a6082e4169f9ad97655ea1c458e
mustafcode10 <mustafdiiriye@hotmail.com>
2018-12-26 18:55:49 +0200

    feat update

--- react-client/src/components/Nav.js
+++ react-client/src/components/Nav.js
@@ -35,9 +35,9 @@ function Nav(props) {
           <Typography variant="h6" color="inherit" className={classes.grow}>
           <Link to= '/'>Home</Link>
           </Typography>
-          <Button color="inherit">
+          {/* <Button color="inherit">
           <Link to ='/Student'>Student</Link>
-          </Button>
+          </Button> */}
           <Button color="inherit">
           <Link to='/signin'>Signin</Link>
           </Button>
