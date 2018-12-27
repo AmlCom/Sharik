9ab6e416c9c181e5b91410ae71fe49ea466a7070
Yahyarashid <yahyomar63@gmail.com>
2018-12-27 16:51:17 +0200

    fix

---
+++ Server_Express/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+    google: {
+    clientID: '95588294200-70b1t1rp78ikrr625q1nlj771ms39umd.apps.googleusercontent.com',
+    clientSecret: 'oKNu2U45osgT1waeMmexzXm8'
+    },
+    facebook: {
+        clientID: '352689985531003',
+        clientSecret: '37447ce938bbd2812186729fd4a79ca9',
+    },
+    mongodb: {
+        dbURI: 'mongodb://sharik:root123@ds139251.mlab.com:39251/zahgan'
+    },
+    session: {
+        cookie: 'erfdvdgerwef'
+    }
+}
