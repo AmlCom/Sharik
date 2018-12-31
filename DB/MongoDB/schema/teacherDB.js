d38e12fd6c7375783149e763c34e7c5eb484f909
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-31 17:04:23 +0200

    (fix) conflict

--- DB/MongoDB/schema/teacherDB.js
+++ DB/MongoDB/schema/teacherDB.js
@@ -1,35 +1,6 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-<<<<<<< HEAD
-// //create profile schema and model for the teacher
-// const TeacherSchema = new Schema({
-//     teacherName: {
-//         type: String,
-//         required: [true, 'Name Field is Required']
-//     }
-
-//     // teacherMajor: {
-//     //     type: String,
-//     //     required: [true, 'Name Field is Required']
-//     // },
-
-//     // info: {
-//     //     type: String,
-//     //     required: [true, 'Name Field is Required']
-//     // },
-
-//     // price: {
-//     //     type: Number,
-//     //     required: [true, 'Name Field is Required']
-//     // },
-
-//     // picURL: {
-//     //     type: String,
-//     //     required: [true, 'Name Field is Required']
-//     // }
-// });
-=======
 //create profile schema and model for the teacher
 const TeacherSchema = new Schema({
     teacherName: {
@@ -57,9 +28,8 @@ const TeacherSchema = new Schema({
         required: [true, 'Name Field is Required']
     }
 });
->>>>>>> f9013ff3de222cc65f721ef5910c530aa3bd64d8
 
 
 
-// var Teacher = mongoose.model('teacher', TeacherSchema);
-// module.exports = Teacher;
+var Teacher = mongoose.model('teacher', TeacherSchema);
+module.exports = Teacher;
