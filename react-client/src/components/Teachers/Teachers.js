9e6d14b9b990b3ca804b669fbd17edb9189c6d13
mustafcode10 <mustafdiiriye@hotmail.com>
2019-01-04 21:56:26 +0200

    update

--- react-client/src/components/Teachers/Teachers.js
+++ react-client/src/components/Teachers/Teachers.js
@@ -1,54 +1,5 @@
 import React, { Component } from 'react';
 import './Teacher.css';
-<<<<<<< HEAD
-import Search from '../search';
-
-
-const Teachers = () => {
-    return (
-        <div className='container'>
-            <h5 className='panel-body col-md-6'>Search for a teacher</h5>
-            {/* <div className="col-md-6">
-                <input className="form-control" type="text" placeholder="Filter Teachers..." />
-            </div> */}
-            <Search />
-
-            <form className='container'>
-                <div class="form-group">
-                <tr>
-                  
-                    <td className='border'>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNimrjRZN3jIxU-D90rCnZ6pZvp9QF4t55QWP6PdOrCcyWpsvT" alt="" className="rounded card" />
-                    <br/>
-                    <br/>
-                    <h6> Teacher Name </h6>
-                    <p> Teacher Major</p>
-                    <br/>
-                    <button type="submit" class="btn btn-primary" href="">Profile</button>
-                    <br/>
-                    <br/>
-                    </td>
-
-                   <div className='spacing'>
-                    <td className='border'>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNimrjRZN3jIxU-D90rCnZ6pZvp9QF4t55QWP6PdOrCcyWpsvT" alt="" className="rounded card" />
-                    <br/>
-                    <h6> Teacher Name</h6>
-                    <br/>
-                    <p> Teacher Major</p>
-                    <br/>
-                    <button type="submit" class="btn btn-primary" href="">Profile</button>
-                    <br/>
-                    <br/>
-                    </td>
-
-                    </div>  
-                </tr>
-                    
-
-                    
-                    
-=======
 import axios from 'axios';
 import Search from '../search'
 
@@ -57,7 +8,6 @@ class Teachers extends Component {
     state = {
         teachers: []
     }
->>>>>>> 178f4731be71aa2c899a82e9a55ad5bcc2063965
 
     componentDidMount() {
         axios.get('get/teacher')
